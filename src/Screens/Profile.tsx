64315625fc5ae9a4a1df601112c97f384621152d
PatrickEinstein <mohammedola1234@gmail.com>
2025-01-13 21:20:11 +0100

    fix

--- src/Screens/Profile.tsx
+++ src/Screens/Profile.tsx
@@ -1,6 +1,35 @@
-import { BiCheck } from "react-icons/bi";
-import { BsPersonFillCheck } from "react-icons/bs";
+import { BiBriefcase, BiCheck, BiHelpCircle } from "react-icons/bi";
+import { BsBank, BsPersonDash, BsPersonFillCheck } from "react-icons/bs";
+import { IoLogOutOutline } from "react-icons/io5";
 import { PiCaretRightLight } from "react-icons/pi";
+
+const profileItems = [
+  {
+    name: "Profile Settings",
+    link: "/",
+    logo: <BsPersonDash />,
+  },
+  {
+    name: "Payment method",
+    link: "/",
+    logo: <BsBank />,
+  },
+  {
+    name: "Privacy Policy",
+    link: "/",
+    logo: <BiBriefcase />,
+  },
+  {
+    name: "Contact us",
+    link: "/",
+    logo: <BiHelpCircle />,
+  },
+  {
+    name: "Logout",
+    link: "/",
+    logo: <IoLogOutOutline />,
+  },
+];
 
 const Profile = () => {
   return (
@@ -15,9 +44,39 @@ const Profile = () => {
         </div>
         <div className="flex flex-col">
           <span className="font-semibold">Olalekan Mohammed</span>
-          <span className="font-light text-slate-400">mohammedola1234@gmail.com</span>
+          <span className="font-light text-slate-400">
+            mohammedola1234@gmail.com
+          </span>
         </div>
-        <PiCaretRightLight className="text-slate-400 text-2xl"/>
+        <PiCaretRightLight className="text-slate-400 text-2xl" />
+      </div>
+
+      <div className="flex flex-row w-full px-5  bg-white mt-10 mx-auto rounded-2xl">
+        <div className="flex flex-col px-4 w-1/2 h-[100px] my-3 border-r-2 gap-3 py-5 border-slate-300">
+          <span className="font-semibold text-3xl">0</span>
+          <span className="text-slate-300 text-xl">Completed gigs</span>
+        </div>
+        <div className="flex flex-col px-4 w-1/2 my-3 h-[100px] gap-3 py-5">
+          <span className="font-semibold text-3xl">NGN 0.00</span>
+          <span className="text-slate-300 text-xl">Total earnings</span>
+        </div>
+      </div>
+      <div className=" flex flex-col mt-8">
+        {profileItems.map(({ name, link, logo }, index) => (
+          <div
+            key={index}
+            className="flex flex-row justify-between items-center p-4 border-b border-gray-200 bg-white py-6"
+          >
+
+            <a href={link} className="flex flex-row items-center space-x-3">
+              <span className="text-3xl">{logo}</span>
+              <span className="font-light">{name}</span>
+            </a>
+
+            {/* Right Section: Icon */}
+            <PiCaretRightLight className="text-slate-400 text-2xl" />
+          </div>
+        ))}
       </div>
     </div>
   );
