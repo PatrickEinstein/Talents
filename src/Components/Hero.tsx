50b9b55e84da7ccb5c9fe78153ab13b74f884cd4
PatrickEinstein <mohammedola1234@gmail.com>
2025-02-25 18:22:22 +0100

    fixed

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,8 +3,6 @@ import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { carouselsImages } from "../../constants";
 
-
-
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -17,31 +15,28 @@ const Hero = () => {
       prevIndex === 0 ? carouselsImages.length - 1 : prevIndex - 1
     );
   };
-//   setInterval(() => {
-//     handleNext();
-//   }, 10000);
+  //   setInterval(() => {
+  //     handleNext();
+  //   }, 10000);
   return (
     <div
       id="home"
       className="relative flex flex-col items-center justify-center h-screen text-center p-6 bg-gray-100 overflow-hidden"
     >
-      {/* Image Carousel */}
       <div className="absolute inset-0 w-full h-full">
         {carouselsImages.map(({ img }, index) => (
-          <div
+          <img
+            src={img}
             key={index}
-            className={`absolute inset-0 bg-cover bg-center bg-no-repeat transition-opacity duration-700 ${
+            className={`absolute inset-0 w-full h-full object-cover transition-opacity duration-700 ${
               index === currentIndex ? "opacity-100" : "opacity-0"
             }`}
-            style={{ backgroundImage: `url(${img})` }}
           />
         ))}
       </div>
 
-      {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-70"></div>
 
-      {/* Navigation Buttons */}
       <button
         onClick={handlePrevious}
         className="absolute top-3/4 left-4 transform -translate-y-1/2 bg-white p-3 rounded-full shadow hover:bg-gray-200 transition"
@@ -57,17 +52,20 @@ const Hero = () => {
 
       {/* Content */}
       <div className="relative z-10 text-white">
-        <h2 className="text-3xl font-bold mb-4">Get Connected To Jobs, Services, Products and Goods</h2>
+        <h2 className="text-3xl font-bold mb-4">
+          Get Connected To Jobs, Services, Products and Goods
+        </h2>
         <p className="text-lg text-gray-200 mb-6">
-          Browse thousands of oppurtunities and connect to everything andd everyone you need.
+          Browse thousands of oppurtunities and connect to everything andd
+          everyone you need.
         </p>
       </div>
-        <Link
-          to="/signup"
-          className="bg-blue-500 absolute bottom-[10%] text-white px-6 py-3 rounded text-lg transition hover:bg-blue-600"
-        >
-          Get Started
-        </Link>
+      <Link
+        to="/signup"
+        className="bg-blue-500 absolute bottom-[10%] text-white px-6 py-3 rounded text-lg transition hover:bg-blue-600"
+      >
+        Get Started
+      </Link>
     </div>
   );
 };
