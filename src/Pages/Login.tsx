e30c2b638a91040062c13ec8e301c87372bb85d7
PatrickEinstein <mohammedola1234@gmail.com>
2025-02-25 19:08:22 +0100

    fix

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,9 +4,12 @@ import { userFetchService } from "../BackendServices/userFetchServices";
 import { LoggedInRes } from "../types";
 import Loader from "../Components/Loader";
 import { GoEyeClosed } from "react-icons/go";
+import { carouselsImages } from "../../constants/constants";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
   const [viewPassword, setViewPassword] = useState<boolean>(false);
   const [formData, setformData] = useState({
     email: "",
@@ -62,47 +65,62 @@ const LoginPage = () => {
     setLoading(false);
   };
   return (
-    <div className="max-w-md mx-auto mt-[25%] md:mt-[10%] p-6 bg-white shadow-lg rounded-lg">
-      <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <div className="flex flex-col gap-4">
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          className="p-2 border rounded"
-          onChange={onHandleChange}
-        />
-        <div className="flex flex-row justify-between items-center">
+    <div className="relative flex flex-col  justify-center h-screen text-center bg-gray-100 overflow-hidden">
+      <div className="inset-0 w-full h-full">
+        {carouselsImages.map(({ img }, index) => (
+          <img
+            src={img}
+            key={index}
+            className={`absolute inset-0 h-full w-full object-cover transition-opacity duration-700 ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        ))}
+      </div>
+
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute p-5 w-screen">
+        <h2 className="text-2xl font-bold mb-4 text-white">Login</h2>
+        <div className="flex flex-col gap-6">
           <input
-            name="password"
-            type={viewPassword ? "text" : "password"}
-            placeholder="Password"
+            type="email"
+            name="email"
+            placeholder="Email"
             required
-            className="p-2 border rounded w-[90%]"
+            className="p-2 border rounded"
             onChange={onHandleChange}
           />
-          <GoEyeClosed onClick={() => setViewPassword((prev) => !prev)} />
+          <div className="flex flex-row justify-between items-center">
+            <input
+              name="password"
+              type={viewPassword ? "text" : "password"}
+              placeholder="Password"
+              required
+              className="p-2 border rounded w-[90%]"
+              onChange={onHandleChange}
+            />
+            <GoEyeClosed onClick={() => setViewPassword((prev) => !prev)} />
+          </div>
+          <button
+            onClick={onLogin}
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Login
+          </button>
         </div>
-        <button
-          onClick={onLogin}
-          className="bg-blue-500 text-white p-2 rounded"
-        >
-          Login
-        </button>
-      </div>
-      <div className="flex flex-row justify-between">
-        <p className="mt-4 text-sm">
-          Don't have an account?{" "}
-          <Link to="/signup" className="text-blue-500">
-            Sign up
-          </Link>
-        </p>
-        <p className="mt-4 text-sm">
-          <Link to="/forgot-password" className="text-blue-500">
-            forgot password?
-          </Link>
-        </p>
+        <div className="flex flex-row justify-between">
+          <p className="mt-4 text-sm text-white ">
+            Don't have an account?{" "}
+            <Link to="/signup" className="text-blue-200">
+              Sign up
+            </Link>
+          </p>
+          <p className="mt-4 text-sm ">
+            <Link to="/forgot-password" className="text-blue-200">
+              forgot password?
+            </Link>
+          </p>
+        </div>
       </div>
       <Loader isLoading={loading} />
     </div>
--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userFetchService } from "../BackendServices/userFetchServices";
 import Loader from "../Components/Loader";
+import { carouselsImages } from "../../constants/constants";
 
 const SignUpInputs = [
   { label: "First Name", name: "firstName", placeholder: "John" },
@@ -30,7 +31,8 @@ const SignUpInputs = [
 ];
 
 const SignupPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [countries, setCountries] = useState<{ name: string }[]>([]);
   const [countrydialCode, setcountrydialCode] = useState<string>("");
@@ -60,16 +62,18 @@ const SignupPage = () => {
     console.log(`createdUserRes==>`, createdUser);
 
     if (createdUser.status === 201) {
-     alert(createdUser.message)
-     setTimeout(() =>{
-      navigate("/login")
-     }, 3000)
+      alert(createdUser.message);
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
     } else {
       alert(createdUser.message);
     }
     setLoading(false);
   };
-  const onHandleChange = (e: React.ChangeEvent<HTMLInputElement|HTMLSelectElement>) => {
+  const onHandleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setformData((prev) => ({ ...prev, [name]: value }));
   };
@@ -146,133 +150,148 @@ const SignupPage = () => {
   }, [formData]);
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-lg">
-      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
-      <div className="flex flex-col gap-4">
-        {SignUpInputs.map(({ label, name, placeholder }) => {
-          if (label === "Nationality") {
-            return (
-              <select
-                key={name}
-                name={name}
-                value={formData.nationality}
-                className="p-2 border rounded"
-                onChange={(e) => {
-                  getAllStateofCountry(e.target.value);
-                  getCountryDialCode(e.target.value);
-                  onHandleChange(e);
-                }}
-              >
-                <option value="">Select a country</option>
-                {countries.map((country) => (
-                  <option key={country.name} value={country.name}>
-                    {country.name}
-                  </option>
-                ))}
-              </select>
-            );
-          }
-
-          if (label === "State Of Residence") {
-            return (
-              <select
-                key={name}
-                name={name}
-                value={formData.state}
-                onChange={(e) => {
-                  getAllCitiesofState(selectedCountry, e.target.value);
-                  onHandleChange(e);
-                }}
-                className="p-2 border rounded"
-              >
-                <option value="">Select a state</option>
-                {states.map((state) => (
-                  <option key={state} value={state}>
-                    {state}
-                  </option>
-                ))}
-              </select>
-            );
-          }
-
-          if (label === "City") {
-            return (
-              <select
-                name={name}
-                value={formData.city}
-                onChange={onHandleChange}
-                key={name}
-                className="p-2 border rounded"
-              >
-                <option value="">Select a city</option>
-                {cities.map((state) => (
-                  <option key={state} value={state}>
-                    {state}
-                  </option>
-                ))}
-              </select>
-            );
-          }
-
-          if (label === "Date of Birth") {
-            return (
-              <input
-                key={label}
-                type="date"
-                name={name}
-                placeholder={placeholder}
-                required
-                className="p-2 border rounded"
-                onChange={onHandleChange}
-              />
-            );
-          }
-
-          if (label === "Phone Number") {
-            return (
-              <input
-                key={label}
-                type="phone"
-                name={name}
-                value={`${countrydialCode}${formData.phone}`}
-                placeholder={placeholder}
-                maxLength={14}
-                required
-                className="p-2 border rounded"
-                onChange={(e) => {
-                  onHandleChange(e);
-                  handlePhoneChange(e);
-                }}
-              />
-            );
-          }
-
-          return (
-            <input
-              key={label}
-              type="text"
-              name={name}
-              placeholder={placeholder}
-              required
-              className="p-2 border rounded"
-              onChange={onHandleChange}
-            />
-          );
-        })}
-        <button
-          onClick={onSignUp}
-          className="bg-blue-500 text-white p-2 rounded"
-        >
-          Sign Up
-        </button>
+    <div className="relative flex flex-col items-center justify-center h-screen text-center bg-gray-100 overflow-hidden">
+      <div className="inset-0 w-full h-full">
+        {carouselsImages.map(({ img }, index) => (
+          <img
+            src={img}
+            key={index}
+            className={`absolute inset-0 h-full w-full object-cover transition-opacity duration-700 ${
+              index === currentIndex ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        ))}
       </div>
-      <p className="mt-4 text-sm">
-        Already have an account?{" "}
-        <Link to="/login" className="text-blue-500">
-          Login
-        </Link>
-      </p>
-      <Loader isLoading={loading}/>
+
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute bottom-5 w-full p-5">
+        <h2 className="text-2xl font-bold mb-4 text-white">Sign Up</h2>
+        <div className="flex flex-col  gap-6">
+          {SignUpInputs.map(({ label, name, placeholder }) => {
+            if (label === "Nationality") {
+              return (
+                <select
+                  key={name}
+                  name={name}
+                  value={formData.nationality}
+                  className="p-2 border rounded"
+                  onChange={(e) => {
+                    getAllStateofCountry(e.target.value);
+                    getCountryDialCode(e.target.value);
+                    onHandleChange(e);
+                  }}
+                >
+                  <option value="">Select a country</option>
+                  {countries.map((country) => (
+                    <option key={country.name} value={country.name}>
+                      {country.name}
+                    </option>
+                  ))}
+                </select>
+              );
+            }
+
+            if (label === "State Of Residence") {
+              return (
+                <select
+                  key={name}
+                  name={name}
+                  value={formData.state}
+                  onChange={(e) => {
+                    getAllCitiesofState(selectedCountry, e.target.value);
+                    onHandleChange(e);
+                  }}
+                  className="p-2 border rounded"
+                >
+                  <option value="">Select a state</option>
+                  {states.map((state) => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </select>
+              );
+            }
+
+            if (label === "City") {
+              return (
+                <select
+                  name={name}
+                  value={formData.city}
+                  onChange={onHandleChange}
+                  key={name}
+                  className="p-2 border rounded"
+                >
+                  <option value="">Select a city</option>
+                  {cities.map((state) => (
+                    <option key={state} value={state}>
+                      {state}
+                    </option>
+                  ))}
+                </select>
+              );
+            }
+
+            if (label === "Date of Birth") {
+              return (
+                <input
+                  key={label}
+                  type="date"
+                  name={name}
+                  placeholder={placeholder}
+                  required
+                  className="p-2 border rounded"
+                  onChange={onHandleChange}
+                />
+              );
+            }
+
+            if (label === "Phone Number") {
+              return (
+                <input
+                  key={label}
+                  type="phone"
+                  name={name}
+                  value={`${countrydialCode}${formData.phone}`}
+                  placeholder={placeholder}
+                  maxLength={14}
+                  required
+                  className="p-2 border rounded"
+                  onChange={(e) => {
+                    onHandleChange(e);
+                    handlePhoneChange(e);
+                  }}
+                />
+              );
+            }
+
+            return (
+              <input
+                key={label}
+                type="text"
+                name={name}
+                placeholder={placeholder}
+                required
+                className="p-2 border rounded"
+                onChange={onHandleChange}
+              />
+            );
+          })}
+          <button
+            onClick={onSignUp}
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Sign Up
+          </button>
+        </div>
+        <p className="mt-4 text-sm text-white">
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-500">
+            Login
+          </Link>
+        </p>
+      </div>
+      <Loader isLoading={loading} />
     </div>
   );
 };
