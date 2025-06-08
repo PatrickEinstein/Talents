834429be9d6da44755e776b7571f043bb4dedad3
PatrickEinstein <mohammedola1234@gmail.com>
2025-06-08 09:26:55 +0100

    solve merge conflict

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,39 +1,5 @@
 export const carouselsImages = [
   {
-<<<<<<< HEAD
-    img: "/assets/img1.jpg",
-  },
-  {
-    img: "/assets/img2.jpg",
-  },
-  {
-    img: "/assets/img3.jpg",
-  },
-  {
-    img: "/assets/img4.jpg",
-  },
-  {
-    img: "/assets/img5.jpg",
-  },
-  {
-    img: "/assets/img6.jpg",
-  },
-  {
-    img: "/assets/img7.jpg",
-  },
-  {
-    img: "/assets/img8.jpg",
-  },
-  {
-    img: "/assets/img9.jpg",
-  },
-  {
-    img: "/assets/img10.jpg",
-  },
-  {
-    img: "/assets/img11.jpg",
-  },
-=======
     img: "/assets/talents1.jpg",
   },
   {
@@ -71,5 +37,4 @@ export const carouselsImages = [
   // {
   //   img: "/assets/img11.jpg",
   // },
->>>>>>> origin/wande
 ];
